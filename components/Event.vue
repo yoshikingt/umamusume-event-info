69ba09466ab27cd345bc7f6d1ed9ef922ccfbe92
yoshikingt <yskngt1007@gmail.com>
2021-05-26 00:03:29 +0900

    first commit

---
+++ components/Event.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <b-form-select v-model="selected" :options="selectArray" @change="handleSelectChanged(selected)" />
+    <b-list-group>
+      <b-list-group-item v-for="description in eventDescriptionsArray" :key="description.title">
+        <div>{{ description.title }}</div>
+        <b-list-group>
+          <b-list-group-item v-for="option in description.options" :key="option.name">
+            <div>{{ option.name }}</div>
+            <div>{{ option.effect }}</div>
+          </b-list-group-item>
+        </b-list-group>
+      </b-list-group-item>
+    </b-list-group>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    selectArray: {
+      type: Array,
+      default: null
+    },
+    eventArray: {
+      type: Array,
+      default: null
+    }
+  },
+  data () {
+    return {
+      selected: null,
+      eventDescriptionsArray: []
+    }
+  },
+  methods: {
+    handleSelectChanged (selected) {
+      this.eventDescriptionsArray = this.eventArray.find(function (event) {
+        return event.id === selected
+      }).descriptions
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <b-tabs justified>
+      <b-tab title="育成ウマ娘" active>
+        <Event :select-array="trainingSelectArray" :event-array="trainingEventArray" />
+      </b-tab>
+      <b-tab title="サポートカード">
+        <Event :select-array="supportSelectArray" :event-array="supportEventArray" />
+      </b-tab>
+    </b-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      trainingSelectArray: [],
+      trainingEventArray: [],
+      supportSelectArray: [],
+      supportEventArray: []
+    }
+  },
+  mounted () {
+    this.trainingSelectArray = require('@/assets/json/training/select.json')
+    this.trainingEventArray = require('@/assets/json/training/event.json')
+    this.supportSelectArray = require('@/assets/json/support/select.json')
+    this.supportEventArray = require('@/assets/json/support/event.json')
+  }
+}
+</script>
+
+<style></style>

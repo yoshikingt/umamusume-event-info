33e872a3379e35b2679c23cda948c2522ddb324c
yoshikingt <yskngt1007@gmail.com>
2021-05-26 11:16:26 +0900

    [modify] 折りたたみに対応

--- components/Event.vue
+++ components/Event.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
     <b-form-select v-model="selected" :options="selectArray" @change="handleSelectChanged(selected)" />
-    <b-list-group>
-      <b-list-group-item v-for="description in eventDescriptionsArray" :key="description.title">
-        <div>{{ description.title }}</div>
-        <b-list-group>
-          <b-list-group-item v-for="option in description.options" :key="option.name">
-            <div>{{ option.name }}</div>
-            <div>{{ option.effect }}</div>
-          </b-list-group-item>
-        </b-list-group>
-      </b-list-group-item>
-    </b-list-group>
+    <b-card-group>
+      <b-card v-for="description in eventDescriptionsArray" :key="description.title" no-body>
+        <b-card-header v-b-toggle="'accordion-' + description.id">
+          {{ description.title }}
+        </b-card-header>
+        <b-collapse :id="'accordion-' + description.id">
+          <b-list-group flush>
+            <b-list-group-item v-for="option in description.options" :key="option.name">
+              <div>{{ option.name }}</div>
+              <div>{{ option.effect }}</div>
+            </b-list-group-item>
+          </b-list-group>
+        </b-collapse>
+      </b-card>
+    </b-card-group>
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -26,6 +26,23 @@ export default {
     this.trainingEventArray = require('@/assets/json/training/event.json')
     this.supportSelectArray = require('@/assets/json/support/select.json')
     this.supportEventArray = require('@/assets/json/support/event.json')
+
+    this.trainingEventArray.forEach((event) => {
+      if (event.descriptions != null) {
+        event.descriptions.forEach((description) => {
+          // アコーディオン用にランダムな文字列を生成（v-b-toggle が食えないので数値はダメ）
+          description.id = Math.random().toString(32).substring(2)
+        })
+      }
+    })
+    this.supportEventArray.forEach((event) => {
+      if (event.descriptions != null) {
+        event.descriptions.forEach((description) => {
+          // アコーディオン用にランダムな文字列を生成（v-b-toggle が食えないので数値はダメ）
+          description.id = Math.random().toString(32).substring(2)
+        })
+      }
+    })
   }
 }
 </script>
